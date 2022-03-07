a63941d5a0a69f2b87f6ea031190bd7cfbbe7562
Fernando <fernandopaucar149@gmail.com>
2022-03-07 17:10:55 -0500

    Create user

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,15 +4,25 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
+CREATE TABLE users(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  password VARCHAR(255)
+);
+
 CREATE TABLE contacts (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR (255),
   phone_number VARCHAR(255)
 );
 
+INSERT INTO
+  users(name, email, password)
+VALUES
+  ('fernando', 'fernandopaucar149@gmail.com', '12345');
+
 CREATE TABLE user_conection(
-
- id INT AUTO_INCREMENT PRIMARY KEY,
- ip VARCHAR(255)
-
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  ip VARCHAR(255)
 );
